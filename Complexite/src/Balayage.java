4578d01faff25dbbb4a3e0e1862aa4fb24e00700
Mervine <mervine.lieffroy7@etu.univ-lorraine.fr>
2017-02-23 23:20:57 +0100

    Mise en place du tri par tas

--- Complexite/src/Balayage.java
+++ Complexite/src/Balayage.java
@@ -7,12 +7,45 @@ public class Balayage {
 		l = new ArrayList<Cercle>();
 	}
 	
-	public void trie(){
-		
+	public void addCercle(Cercle c){
+		l.add(c);
 	}
 	
-	public void comparaison(){
-		
+	public void tamiser(int i, int longueur){
+		Cercle change;
+		int k = i;
+		int j = 2*k;
+		while ( j < longueur ){
+			if (j < longueur && l.get(j).x < l.get(k).x){
+				j++;
+			}
+			if ( l.get(k).x < l.get(j).x){
+				//échanger
+				change=l.get(k);
+				l.set(k, l.get(j));
+				l.set(j, change);
+				
+				
+				k=j;
+				j=2*k;
+			}
+			else{j=longueur;}
+		}
+	}
+	
+	public void triParTas(){
+		Cercle change;
+		for(int i = (l.size()-1)/2 ; i >= 0 ; i--){
+			tamiser(i, l.size()-1);
+		}
+		for(int i = l.size()-1 ; i > 1 ; i--){
+			//échanger
+			change=l.get(i);
+			l.set(i, l.get(1));
+			l.set(1, change);
+			
+			tamiser(1, i-1);
+		}
 	}
 
 }
--- Complexite/src/Main.java
+++ Complexite/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 
 
-		ToutesLesPaires test1 = new ToutesLesPaires();
+		/*ToutesLesPaires test1 = new ToutesLesPaires();
 		test1.addCercle(new Cercle (3,3,2));
 		test1.addCercle(new Cercle (4,7,3));
 		test1.addCercle(new Cercle (10,5,4));
@@ -12,7 +12,25 @@ public class Main {
 		test1.addCercle(new Cercle (8,12,1));
 		test1.addCercle(new Cercle (9,13,1));
 		
-		test1.comparaison();
+		System.out.println("Résultat de la comparaison par paire :");
+		test1.comparaison();*/
+		
+		
+		
+		Balayage test2 = new Balayage();
+		test2.addCercle(new Cercle (3,3,2));
+		test2.addCercle(new Cercle (4,7,3));
+		test2.addCercle(new Cercle (10,5,4));
+		test2.addCercle(new Cercle (6,3,2));
+		test2.addCercle(new Cercle (8,12,1));
+		test2.addCercle(new Cercle (9,13,1));
+		
+		test2.triParTas();
+		
+		System.out.println("Résultat du tri par tas :");
+		for(Cercle c:  test2.l){
+			System.out.println(c.num);
+		}
 
 	}
 
