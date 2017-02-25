e53a68404e13cd1d7355917f3271217a5816c096
Mervine <mervine.lieffroy7@etu.univ-lorraine.fr>
2017-02-25 13:58:12 +0100

    Implémentation du balayage

--- Complexite/src/Balayage.java
+++ Complexite/src/Balayage.java
@@ -12,18 +12,18 @@ public class Balayage {
 	}
 	
 	public void tamiser(int i, int longueur){
-		Cercle change;
+		Cercle echange;
 		int k = i;
 		int j = 2*k;
 		while ( j < longueur ){
-			if (j < longueur && l.get(j).x < l.get(k).x){
+			if (j < longueur && l.get(j).x - l.get(j).rayon < l.get(k).x - l.get(k).rayon){
 				j++;
 			}
-			if ( l.get(k).x < l.get(j).x){
+			if ( l.get(k).x - l.get(k).rayon < l.get(j).x - l.get(j).rayon){
 				//échanger
-				change=l.get(k);
+				echange=l.get(k);
 				l.set(k, l.get(j));
-				l.set(j, change);
+				l.set(j, echange);
 				
 				
 				k=j;
@@ -34,18 +34,37 @@ public class Balayage {
 	}
 	
 	public void triParTas(){
-		Cercle change;
+		Cercle echange;
 		for(int i = (l.size()-1)/2 ; i >= 0 ; i--){
 			tamiser(i, l.size()-1);
 		}
 		for(int i = l.size()-1 ; i > 1 ; i--){
 			//échanger
-			change=l.get(i);
+			echange=l.get(i);
 			l.set(i, l.get(1));
-			l.set(1, change);
+			l.set(1, echange);
 			
 			tamiser(1, i-1);
 		}
 	}
-
+	
+	public double distance(Cercle c1, Cercle c2){
+		return Math.sqrt( Math.pow(c2.getX()- c1.getX(), 2) + Math.pow(c2.getY()- c1.getY(), 2) );
+	}
+	
+	public void comparaison(){
+		for(int i = 0 ; i < l.size() ; i++){
+			for(int j = i+1 ; j < l.size() ; j++){
+				if(l.get(j).x - l.get(j).rayon > l.get(i).x + l.get(i).rayon  ){
+					j = l.size();
+				}
+				else{
+					//Tester si les disques se croisent
+					if(distance(l.get(i),l.get(j)) < l.get(i).getRayon() + l.get(j).getRayon()){
+						System.out.println("Le cercle ["+ l.get(i).getNum() +"] croise ["+ l.get(j).getNum() +"]");
+					}	
+				}
+			}
+		}
+	}
 }
